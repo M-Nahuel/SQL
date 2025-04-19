2c9f3a00da5c7a22387874e8116d3c2e1ef0e21a
M_Nahuel <91757112+M-Nahuel@users.noreply.github.com>
2025-04-18 21:28:38 -0400

    Product Price at a Given Date [Performance Tunned with Window Functions]

--- sql-50/ADVANCED_SELECT_AND_JOINS/1164.sql
+++ sql-50/ADVANCED_SELECT_AND_JOINS/1164.sql
@@ -10,15 +10,38 @@ insert into Products (product_id, new_price, change_date) values ('3', '20', '20
 # Write your MySQL query statement below
 
 
-SELECT prod.product_id,
-    COALESCE((
-        SELECT new_price
-        FROM Products
-        WHERE product_id = prod.product_id AND change_date <= '2019-08-16'
-        ORDER BY change_date DESC
-        LIMIT 1),
-        10) price
-FROM (
+-- SELECT prod.product_id,
+--     COALESCE((
+--         SELECT new_price
+--         FROM Products
+--         WHERE product_id = prod.product_id AND change_date <= '2019-08-16'
+--         ORDER BY change_date DESC
+--         LIMIT 1),
+--         10) price
+-- FROM (
+--     SELECT DISTINCT(product_id)
+--     FROM Products
+-- ) prod;
+
+WITH sorted AS (
+    SELECT *,
+        ROW_NUMBER() OVER (
+            PARTITION BY product_id
+            ORDER BY change_date DESC
+        ) num
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+),
+latest AS (
+    SELECT product_id, new_price
+    FROM sorted
+    WHERE num = 1
+),
+products AS (
     SELECT DISTINCT(product_id)
     FROM Products
-) prod;
+)
+SELECT prod.product_id, 
+COALESCE(last.new_price,10) price
+FROM products prod
+LEFT JOIN latest last ON prod.product_id = last.product_id;
